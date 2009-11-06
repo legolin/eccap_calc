c8e1a859fbdcc47d1b0e3d0cbb21f12676eb3abc
Larry Siden <lsiden@agentsmith.(none)>
2009-11-05 22:28:05 -0500

    initial commit

---
+++ apps/eccap_calc/controllers/calc_data.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   EccapCalc.calcDataController
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals EccapCalc */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.ObjectController
+*/
+EccapCalc.calcDataController = SC.ObjectController.create(
+/** @scope EccapCalc.calcDataController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
