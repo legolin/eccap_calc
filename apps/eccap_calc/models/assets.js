00e233b83c2bfe0b9a54e67da09b9279b9bc9f4c
Larry Siden <lsiden@gmail.com>
2009-11-17 16:55:58 -0500

    No scrollbar.  Cannot edit Description column.
    Clicking on Description column does not select row or create in-line editor.
    
    There is no scrollbar.  Adding new posts to ledger extends the ledger downward,
    covering the Total.

--- apps/eccap_calc/models/assets.js
+++ apps/eccap_calc/models/assets.js
@@ -14,6 +14,6 @@
 EccapCalc.Asset = SC.Record.extend(
 /** @scope EccapCalc.Assets.prototype */ {
 
-  // TODO: Add your own code here.
-
+    description: SC.Record.attr(String),
+    value: SC.Record.attr(Number),
 }) ;
