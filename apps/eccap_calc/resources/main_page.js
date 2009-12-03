d14f89e0fc3251f8ce769644a7074b9e479c3e2f
Larry Siden <lsiden@agentsmith.(none)>
2009-12-02 22:37:49 -0500

    Trying to get main scrollbar to work

--- apps/eccap_calc/resources/main_page.js
+++ apps/eccap_calc/resources/main_page.js
@@ -15,16 +15,21 @@ EccapCalc.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView assetsView incomeView expensesView'.w(),
+    childViews: 'scrollView'.w(),
+    scrollView: SC.ScrollView.design({
+      contentView: SC.View.design({
+				childViews: 'labelView assetsView incomeView expensesView'.w(),
 
-    labelView: SC.LabelView.design({
-      layout: { left: 100, top: 0, height: 50},
-      displayValue: 'Personal',
-      //fontWeight: SC.BOLD_WEIGHT,
-      tagName: 'h1',
-    }),
-    assetsView: EccapCalc.LedgerView(100, 100, 'EccapCalc.assetsLedgerController'),
-    incomeView: EccapCalc.LedgerView(100, 350, 'EccapCalc.incomeLedgerController'),
-    expensesView: EccapCalc.LedgerView(100, 600, 'EccapCalc.expensesLedgerController'),
+				labelView: SC.LabelView.design({
+					layout: { left: 100, top: 0, height: 50},
+					displayValue: 'Personal',
+					//fontWeight: SC.BOLD_WEIGHT,
+					tagName: 'h1',
+				}),
+				assetsView: EccapCalc.LedgerView(100, 100, 'EccapCalc.assetsLedgerController'),
+				incomeView: EccapCalc.LedgerView(100, 350, 'EccapCalc.incomeLedgerController'),
+				expensesView: EccapCalc.LedgerView(100, 600, 'EccapCalc.expensesLedgerController'),
+			}),
+		}),
   }),
 });
