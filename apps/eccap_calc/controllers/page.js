1c2c11b0540c8713c9e6a04013c9275319ad51cf
Larry Siden <lsiden@gmail.com>
2009-12-04 19:10:57 -0500

    Refactored duplicate code.

--- apps/eccap_calc/controllers/page.js
+++ apps/eccap_calc/controllers/page.js
@@ -20,23 +20,26 @@ EccapCalc.pageController = SC.ObjectController.create(
     this.set('nowShowing', 'personalPage');
   },
 
-  showFacilityPage1: function() {
-    var page = EccapCalc.store.find(EccapCalc.FacilityPage, 'facility-1'); 
+  showFacilityPage: function(facility_id, initial_costs_ledger_id, monthly_costs_ledger_id) {
+    var page = EccapCalc.store.find(EccapCalc.FacilityPage, facility_id); 
     EccapCalc.pageController.set('content', page);
-    EccapCalc.initialFacilityCostsController.set('content', EccapCalc.findLedgerEntries('facility-1-initial'));
-    EccapCalc.initialFacilityCostsController.ledger_id = 'facility-1-initial';
-    EccapCalc.monthlyFacilityCostsController.set('content', EccapCalc.findLedgerEntries('facility-1-monthly'));
-    EccapCalc.monthlyFacilityCostsController.ledger_id = 'facility-1-monthly';
+
+    var initial_cost_entries = EccapCalc.findLedgerEntries(initial_costs_ledger_id);
+    EccapCalc.initialFacilityCostsController.set('content', initial_cost_entries);
+    EccapCalc.initialFacilityCostsController.ledger_id = initial_costs_ledger_id;
+
+    var monthly_cost_entries = EccapCalc.findLedgerEntries(monthly_costs_ledger_id);
+    EccapCalc.monthlyFacilityCostsController.set('content', monthly_cost_entries);
+    EccapCalc.monthlyFacilityCostsController.ledger_id = monthly_costs_ledger_id;
+  },
+
+  showFacilityPage1: function() {
+    this.showFacilityPage('facility-1', 'facility-1-initial', 'facility-1-monthly');
     this.set('nowShowing', 'facilityPage');
   },
 
   showFacilityPage2: function() {
-    var page = EccapCalc.store.find(EccapCalc.FacilityPage, 'facility-2'); 
-    EccapCalc.pageController.set('content', page);
-    EccapCalc.initialFacilityCostsController.set('content', EccapCalc.findLedgerEntries('facility-2-initial'));
-    EccapCalc.initialFacilityCostsController.ledger_id = 'facility-2-initial';
-    EccapCalc.monthlyFacilityCostsController.set('content', EccapCalc.findLedgerEntries('facility-2-monthly'));
-    EccapCalc.monthlyFacilityCostsController.ledger_id = 'facility-2-monthly';
+    this.showFacilityPage('facility-2', 'facility-2-initial', 'facility-2-monthly');
     this.set('nowShowing', 'facilityPage');
   },
 
