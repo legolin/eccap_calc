595b72394c5917cfe4fac7d8b93cf7e90997d111
Larry Siden <lsiden@gmail.com>
2009-12-06 21:24:31 -0500

    Fixed coding errors in literal object array.
    Placed commas inside guids, as in '1,', ...

--- apps/eccap_calc/fixtures/ledger_entry.js
+++ apps/eccap_calc/fixtures/ledger_entry.js
@@ -7,67 +7,67 @@
 sc_require('models/ledger_entry');
 
 EccapCalc.LedgerEntry.FIXTURES = [{
-    guid: '1,',
+    guid: '1',
     description: '401K',
     amount: '150000',
     ledger: 'assets',
   }, {
-    guid: '2,',
+    guid: '2',
     description: 'Home',
     amount: '400000',
     ledger: 'assets',
   }, {
-    guid: '3,',
+    guid: '3',
     description: 'Pension',
     amount: '1500',
     ledger: 'income',
   }, {
-    guid: '4,',
+    guid: '4',
     description: 'Social Security',
     amount: '1200',
     ledger: 'income',
   }, {
-    guid: '5,',
+    guid: '5',
     description: 'Home Insurance',
     amount: '2000',
     ledger: 'expenses',
   }, {
-    guid: '6,',
+    guid: '6',
     description: 'Property Taxes',
     amount: '750',
     ledger: 'expenses',
   }, {
-    guid: '7,',
+    guid: '7',
     description: 'Medical Insurance',
     amount: '800',
     ledger: 'expenses',
   }, {
-    guid: '8,',
+    guid: '8',
     description: 'Initial Deposit',
     amount: '5000',
     ledger: 'facility-1-initial',
   }, {
-    guid: '9,',
+    guid: '9',
     description: 'Rent',
     amount: '2000',
     ledger: 'facility-1-monthly',
   }, {
-    guid: '10,',
+    guid: '10',
     description: 'Meals',
     amount: '1500',
     ledger: 'facility-1-monthly',
   }, {
-    guid: '11,',
+    guid: '11',
     description: 'Initial Deposit',
     amount: '8000',
     ledger: 'facility-2-initial',
   }, {
-    guid: '12,',
+    guid: '12',
     description: 'Rent',
     amount: '2500',
     ledger: 'facility-2-monthly',
   }, {
-    guid: '13,',
+    guid: '13',
     description: 'Meals',
     amount: '1800',
     ledger: 'facility-2-monthly',
